75b21c04721ab8ce370d6f768496c7dd98c37ad7
maruhxn <maruhan1016@gmail.com>
2025-04-02 15:58:53 +0900

    Feat(#3): Post 데이터 Initializer 추가
    - 페이징 조회 테스트를 위해 임시 데이터를 자동으로 추가하는 클래스 생성

---
+++ src/test/java/com/example/guestbook/domain/domain/post/data/DataInitializer.java
@@ -0,0 +1,58 @@
+package com.example.guestbook.domain.domain.post.data;
+
+import com.example.guestbook.domain.post.entity.Post;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@SpringBootTest
+public class DataInitializer {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Autowired
+    TransactionTemplate tx;
+
+    static int BULK_INSERT_SIZE = 1000;
+    static final int EXECUTION_COUNT = 100;
+
+    @Test
+    void init() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        CountDownLatch latch = new CountDownLatch(EXECUTION_COUNT);
+
+        for (int i = 0; i < EXECUTION_COUNT; i++) {
+            executorService.submit(() -> {
+                insert();
+                latch.countDown();
+                System.out.println("latch.getCount() = " + latch.getCount());
+            });
+        }
+
+        latch.await();
+        executorService.shutdown();
+    }
+
+    void insert() {
+        tx.executeWithoutResult(transactionStatus -> {
+            for (int i = 0; i < BULK_INSERT_SIZE; i++) {
+                Post post = Post.builder()
+                        .content("content" + i)
+                        .username("user" + i)
+                        .password("password")
+                        .emotion("HAPPY")
+                        .build();
+
+                em.persist(post);
+            }
+        });
+    }
+}
