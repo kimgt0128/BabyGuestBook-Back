e39297c68e10a84cff395312ea836f940391f938
maruhxn <maruhan1016@gmail.com>
2025-04-02 18:05:28 +0900

    Refactor(#3): 사용하지 않는 메서드 제거

--- src/main/java/com/example/guestbook/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/guestbook/domain/post/repository/PostRepository.java
@@ -2,24 +2,6 @@ package com.example.guestbook.domain.post.repository;
 
 import com.example.guestbook.domain.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    @Query(
-            value = "select * from posts " +
-                    "order by id desc limit :limit",
-            nativeQuery = true
-    )
-    List<Post> findAllInfiniteScroll(@Param("limit") Long limit);
-
-    @Query(
-            value = "select * from posts " +
-                    "where id < :lastPostId " +
-                    "order by id desc limit :limit",
-            nativeQuery = true
-    )
-    List<Post> findAllInfiniteScroll(@Param("limit") Long limit, @Param("lastPostId") Long lastPostId);
 }
