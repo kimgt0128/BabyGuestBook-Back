451a6c531494bdad6af3c8978c1c3ae86f4736b7
KKT <kimgt2828@gmail.com>
2025-04-08 00:51:49 +0900

    Feat(#14): 좋아요 관련 오류 메시지 추가 - LikeErrorCode: 좋아요 관련 오류 메시지 추가 - LikeService: 좋아요 관련 오류 메시지 추가

--- src/main/java/com/example/guestbook/domain/like/service/LikeService.java
+++ src/main/java/com/example/guestbook/domain/like/service/LikeService.java
@@ -1,8 +1,12 @@
 package com.example.guestbook.domain.like.service;
 
 import com.example.guestbook.domain.like.entity.Likes;
+import com.example.guestbook.domain.like.exception.LikeErrorCode;
 import com.example.guestbook.domain.like.repository.LikeRepository;
+import com.example.guestbook.domain.post.exception.PostErrorCode;
 import com.example.guestbook.domain.post.repository.PostRepository;
+import com.example.guestbook.global.error.exception.BadRequestException;
+import com.example.guestbook.global.error.exception.NotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,14 +23,14 @@ public class LikeService {
     public void like(Long memberId, Long postId) {
         // 게시물 존재 확인
         if(!postRepository.existsById(postId)) {
-            return;
+            throw new NotFoundException(PostErrorCode.NOT_FOUND);
         }
 
         // 이미 좋아요 눌렀는지 확인
         Optional<Likes> isExist = likeRepository.findByMemberIdAndPostId(memberId, postId);
 
         if(isExist.isPresent()) {
-            return;
+            throw new BadRequestException(LikeErrorCode.ALREADY_LIKED);
         }
         likeRepository.save(new Likes(memberId, postId));
     }
@@ -34,10 +38,13 @@ public class LikeService {
     @Transactional
     public void unlike(Long memberId, Long postId) {
         Optional<Likes> isExist = likeRepository.findByMemberIdAndPostId(memberId, postId);
+
+        // 좋아요가 존재할 경우에만 취소
         if(isExist.isPresent()) {
             likeRepository.delete(isExist.get());
             return;
         }
+        throw new NotFoundException(LikeErrorCode.NOT_FOUND);
 
     }
 }
